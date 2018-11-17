c4be6bd81c13240566551aa513c5d2175f6962da
PB15020763 <2982693577@qq.com>
2018-11-17 17:58:10 +0800

    pwm-speedcontrol

---
+++ linear_actuator_speed_control/linear_actuator_speed_control.ino
@@ -0,0 +1,83 @@
+#define length 3.0
+#include<math.h>
+int input;
+int input1=9;
+int input2=10;
+int time=1;
+
+float Position(){
+  return(length-analogRead(A0)*length/1023.0);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  pinMode(input1,OUTPUT);
+  pinMode(input2,OUTPUT);
+    
+}
+
+
+float speed=0;    //record speed
+
+void Protect(){
+  if((Position()<=0.08&&speed<0)||(Position()>=2.92&&speed>0))  //protect
+    Speedwrite(input1,input2,0);
+}
+
+void Speedwrite(int input1,int input2,float v){
+  if(v>=0){
+    analogWrite(input2,v/5*255);
+    analogWrite(input1,0);
+    speed=v/5*255;
+  }
+  else{
+    analogWrite(input1,-v/5*255);
+    analogWrite(input2,0);
+    speed=v/5*255;
+  }
+}
+
+float settime=0.5;
+
+float Movetest(float t){     //test   hahahahahaha
+  float spe;
+  float distance=2.6;
+  spe=distance*(-t*2+settime)/(1.0/4*settime*settime);
+  return spe;
+}
+
+void loop() {
+  float a;
+  float t;
+  if(Serial.available()>0){
+    a=Serial.parseFloat();
+    Speedwrite(input1,input2,a);
+    /*if(a==1){
+      analogWrite(input1,250);
+      analogWrite(input2,50);
+      //delay(1000);
+      Serial.println(1);
+    }
+    if(a==2){
+      analogWrite(input1,125);
+      analogWrite(input2,155);
+      Serial.println(2);
+      //delay(1000);
+    }
+    if(a==3){
+      analogWrite(input1,0);
+      analogWrite(input2,0);
+      Serial.println(3);
+    }
+    */
+    
+    while(Serial.read()>=0){}
+  }
+  Protect();
+  
+ 
+  Serial.println(Position());
+}
+  // put your main code here, to run repeatedly:
